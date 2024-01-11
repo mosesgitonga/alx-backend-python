b563065493801ceb0a1bbf726beda8377b91dcef
mosesgitonga <infosec947@gmail.com>
2024-01-11 12:22:34 +0300

    find sum

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from typing import List
+"""
+returning sum in an iterable
+"""
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    returning sum
+    """
+    return sum(input_list)
