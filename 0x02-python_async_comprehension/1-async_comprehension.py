f750417938b4871cf8b1e17b9c69b80ce5a178a3
mosesgitonga <infosec947@gmail.com>
2024-01-16 15:21:14 +0300

    fixing coding style

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 import asyncio
 from typing import AsyncGenerator, List
+async_generator = __import__('0-async_generator').async_generator
 """
 async coprehension
 """
-async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
