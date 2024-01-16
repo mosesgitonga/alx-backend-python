28a2dbbfcebb301a9402f0259f1f531bb135a6ed
mosesgitonga <infosec947@gmail.com>
2024-01-16 15:04:40 +0300

    change

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -11,8 +11,6 @@ async def async_generator() -> AsyncGenerator[float, None]:
     """
     return a list of random nums
     """
-    res = [random.uniform(0, 10) for _ in range(10)]
-    await asyncio.sleep(1)
-    for item in res:
-        await asyncio.sleep(1)
-        yield item
+    for _ in range(0, 10): 
+        await sleep(1)
+        yield uniform(0, 10)
---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import asyncio
+from typing import AsyncGenerator
+"""
+async coprehension
+"""
+
+
+async_generator = __import__('0-async_generator').async_generator
+
+async def async_comprehension() -> AsyncGenerator:
+    """
+    corutine for async comprehension
+    """
+    res = [i for i in async_generator()]
+    return res
