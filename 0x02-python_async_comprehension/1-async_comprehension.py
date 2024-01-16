2f6c8de0f3fbe099044f37cf2be1404f9138bbad
mosesgitonga <infosec947@gmail.com>
2024-01-16 15:17:33 +0300

    fixed pycodestyle

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -4,9 +4,8 @@ from typing import AsyncGenerator
 """
 async coprehension
 """
-
-
 async_generator = __import__('0-async_generator').async_generator
+
 
 async def async_comprehension() -> AsyncGenerator:
     """
