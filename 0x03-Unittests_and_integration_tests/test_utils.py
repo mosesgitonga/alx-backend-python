0a7b77d957823015b60eb2bad231249a41fb9a65
mosesgitonga <infosec947@gmail.com>
2024-02-04 07:15:29 +0300

    correcting key

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -8,36 +8,15 @@ from typing import Mapping, Sequence, Any
 from utils import access_nested_map
 
 
-def access_nested_map(nested_map: Mapping, path: Sequence) -> Any:
-    """Access nested map with key path.
-    Parameters
-    ----------
-    nested_map: Mapping
-        A nested map
-    path: Sequence
-        a sequence of key representing a path to the value
-    Example
-    -------
-    >>> nested_map = {"a": {"b": {"c": 1}}}
-    >>> access_nested_map(nested_map, ["a", "b", "c"])
-    1
-    """
-    for key in path:
-        if not isinstance(nested_map, Mapping):
-            raise KeyError(key)
-        nested_map = nested_map[key]
-
-    return nested_map
-
 class TestAccessNestedMap(unittest.TestCase):
+    """
+    path output
+    """
     @parameterized.expand([
         ({"a": 1}, {"a",}, 1), 
-        ({"a": {"b": 2}}, {"a",}, {"b": 2}),
-        ({"a": {"b": 2}}, {"a", "b"}, 2)
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
         ])
     def test_access_nested_map(self, nested_map, path, expected):
         res = access_nested_map(nested_map, path)
         self.assertEqual(res, expected)
-
-if __name__ == "__main__":
-    unittest.main()
